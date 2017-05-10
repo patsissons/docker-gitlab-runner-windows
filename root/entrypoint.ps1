c20d9b4b0c355e2fce24d64d5389d7da56fc8420
Pat Sissons <pat@marinels.com>
2017-05-09 19:34:41 -0700

    using write-output instead of echo uncommenting the gitlab-runner register call

--- root/entrypoint.ps1
+++ root/entrypoint.ps1
@@ -6,15 +6,15 @@ $exePath = Join-Path $installPath $exeName
 
 If (!(Get-Service $serviceName -ErrorAction SilentlyContinue))
 {
-    echo "installing $serviceName service..."
+    Write-Output "installing $serviceName service..."
     & $exePath install
 
-    echo "registering $serviceName agent..."
-    # & $exePath register --non-interactive
+    Write-Output "registering $serviceName agent..."
+    & $exePath register --non-interactive
 }
 
-echo "Starting up $serviceName service..."
+Write-Output "Starting up $serviceName service..."
 Start-Service -Name $serviceName
 
-echo "Setting up watcher for $serviceName service..."
+Write-Output "Setting up watcher for $serviceName service..."
 & .\Wait-Service.ps1 -ServiceName $serviceName
