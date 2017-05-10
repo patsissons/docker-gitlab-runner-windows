7707c93d0472349668d9923f8d6df714899d604c
Pat Sissons <pat@marinels.com>
2017-05-09 19:07:39 -0700

    adding some helper scripts

---
+++ scripts/build-develop.bat
@@ -0,0 +1,10 @@
+@ECHO OFF
+@REM This updates our working directory to the same directory this script lives in
+PUSHD "%~dp0"
+
+SET TAG=patsissons/gitlab-runner-windows:develop
+
+docker build -t "%TAG%" ..
+
+@REM Restore the previous working directory
+POPD
